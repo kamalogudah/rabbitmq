e28c686a619f9c5d815029180016b636df261f43
kamalogudah <mcpaul2058@gmail.com>
2019-03-02 11:17:55 +0300

    Ensuring Fair Dispatch

--- new_task.rb
+++ new_task.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 require 'bunny'
-connection = Bunny.new
+connection = Bunny.new(automatically_recover: false)
 connection.start
 channel = connection.create_channel
-queue = channel.queue('hello')
+queue = channel.queue('task_queue', durable: true)
 # channel.default_exchange.publish('Hello World!', routing_key: queue.name)
 # puts " [x] Sent 'Hello World!'"
 message = ARGV.empty? ? 'Hello World!' : ARGV.join(' ')
